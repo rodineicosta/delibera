b52cc56d0538afd27207365351dfb04520f1f312
Jacson Passold <jacson@ethymos.com.br>
2016-07-12 04:31:48 -0300

    add comment and media list

--- themes/generic/page-author.php
+++ themes/generic/page-author.php
@@ -33,7 +33,7 @@ global $user_display;
 						<div class="comments-list">
 							<span class="comments-list-title"><?php _e('Comments'); ?></span>
 							<?php
-								$comments = get_comments(array('user_id' => $user->ID));
+								$comments = get_comments(array('user_id' => $user->ID, 'number' => 3));
 								
 								/* @var $comment WP_Comment */
 								foreach ($comments as $comment)
@@ -54,7 +54,64 @@ global $user_display;
 								}
 							?>
 						</div>
-					</div>
+						<div class="comment-view-all">
+							<a class="link-view-all-comments" href="<?php echo get_site_url()?>/delibera/<?php echo deliberaEncryptor('encrypt', $user->ID); ?>/comentarios">
+								<?php _e('Ver Todos' , 'delibera'); ?>
+							</a>
+						</div>
+					</div> <!-- comments-col -->
+					<div class="medias-col">
+						<div class="medias-list">
+							<span class="medias-list-title"><?php _e('Documentos'); ?></span>
+							<?php
+								$unsupported_mimes  = array( 'image/jpeg', 'image/gif', 'image/png', 'image/bmp', 'image/tiff', 'image/x-icon' );
+								$all_mimes          = get_allowed_mime_types();
+								$accepted_mimes     = array_diff( $all_mimes, $unsupported_mimes );
+								$args = array(
+									'author'			=> $user->ID,
+									'post_status'		=> 'publish',
+									'posts_per_page'	=> 3,
+									'post_type'			=> 'attachment',
+									'paged'				=> get_query_var( 'paged' ),
+									'post_mime_type'	=> $accepted_mimes
+								);
+								$medias = new WP_Query($args);
+								
+								/* @var $media WP_Post */
+								if($medias->have_posts())
+								while ($medias->have_posts())
+								{
+									$medias->the_post();
+									global $post;
+									$media = $post;
+									?>
+									<div class="entry-media">
+										<div class="entry-date">
+											<span class="media-cats"><?php echo get_the_category_list(); ?></span>
+											<span class="media-date"><?php the_date() ?></span>
+										</div>
+										<div class="media-post-title">
+											<?php the_title() ?>
+										</div><?php
+										if(is_user_logged_in()) // TODO delete file postback/callback
+										{?>
+											<div class="media-delete">
+												<?php echo wp_get_attachment_link( get_the_ID(), '' , false, false, __("Delete"));  ?>
+											</div><?php
+										}?>
+										<div class="media-download">
+											<?php echo wp_get_attachment_link( get_the_ID(), '' , false, false, __("Download"));  ?>
+										</div>
+									</div><?php
+								}
+							?>
+						</div>
+						<div class="media-view-all">
+							<a class="link-view-all-medias" href="<?php echo get_post_type_archive_link('attachment'); ?>">
+								<?php _e('Ver Todos' , 'delibera'); ?>
+							</a>
+						</div>
+					</div> <!-- medias-col -->
 				</div><!-- .delibera-autor-page -->
 			</main><!-- #main -->
 		</div>
