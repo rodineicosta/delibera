c485a720869f389477c1da16404acab1306b0f99
Jacson Passold <jacson@ethymos.com.br>
2017-05-25 12:57:17 -0300

    vacilei com o parenteses e estou com muita vergonha

--- delibera_notificar.php
+++ delibera_notificar.php
@@ -692,7 +692,7 @@ function delibera_notificar_representantes($mensage, $tipo, $post = false, $user
 			$seguiram = delibera_get_quem_seguiu($post->ID, 'ids');
 		}
 		$paged = 1;
-		while(count($users > 0))
+		while(count($users) > 0)
 		{
 			foreach ($users as $user)
 			{
